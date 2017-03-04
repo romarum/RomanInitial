f21e12e278afc4188cb5b2065e986ac11ab4bf2e
pushapsaini <pushapsaini@users.noreply.github.com>
2017-03-03 20:10:50 -0800

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -241,7 +241,7 @@ def move():
         path_length = len(tentative_path)
         snek_length = len(snek_coords) + 1
 		
-		dead = False
+        dead = False
         
         #logic where the snake drops the food because of enemy's distance
         for enemy in data['snakes']:
