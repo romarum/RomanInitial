810ff03af90c444d4eecfea62ff59ca278350c37
romarum <romarum@yahoo.ca>
2018-02-18 14:00:39 -0800

    refactor 1st

--- app/main.py
+++ app/main.py
@@ -6,86 +6,66 @@ import os
 import sys
 
 SNEK_BUFFER = 3
+
 SNAKE = 1
 WALL = 2
-FOOD = 5
-GOLD = 7
 SAFTEY = 3
 
+FOOD = 5
+GOLD = 7
 
-def direction(from_cell, to_cell):
-    dx = to_cell[0] - from_cell[0]
-    dy = to_cell[1] - from_cell[1]
-    print ('FROM CELL ',from_cell)
-    print ('TO CELL ',to_cell)
-    print ('DX=',dx, ' DY= ',dy)
-    if dx == 1:
-        print('RIGHT')
-        return 'right'
-    elif dx == -1:
-        print('LEFT')
-        return 'left'
-    elif dy == -1:
-        print('UP')
-        return 'up'
-    elif dy == 1:
-        print('DOWN')
-        return 'down'
-
-def distance(p, q):
-    dx = abs(p[0] - q[0])
-    dy = abs(p[1] - q[1])
-    return dx + dy;
-
-def closest(items, start):
-    closest_item = None
-    closest_distance = 10000
-
-    # TODO: use builtin min for speed up
-    for item in items:
-        item_distance = distance(start, item)
-        if item_distance < closest_distance:
-            closest_item = item
-            closest_distance = item_distance
-
-    return closest_item
+grid = []
+otherSnakes = []
+allSnakes = []
+mySnake = NULL
+width = 0
+height = 0
+myHealth = 100
+mySnakeId = ''
 
 
-def init(data):
-    print('data is ', data)
-    enemysnakes=[];
-    enemysnakes=[];
-    mysnakeID = data['you']['id']
-    snakes  = data['snakes']
-    print('snakes are ', snakes)
-    width  = data ['width']
-    height = data ['height']
-    print('width ', width)
-    print('height ', height)
+def init(postData):
+    global width
+    global height
+    global grid 
+    global otherSnakes
+    global mySnake
+    global myHealth
+    global mySnakeId
+    
+    data = postData
+
+    width = data['width']
+    height = data['height']
+    mySnakeID = data['you']['id']
+
+
     grid = [[0 for col in xrange(height)] for row in xrange(width)]
-    for snek in data['snakes']['data']:
-        snekCoords = []
-        if snek['id']==mysnakeID:
-            mysnake = snek
-            print('My snake is ', mysnake)
+    allSnakes = data['snakes']
+    foods= data['food']['data']
+    for snake in allSnakes:
+        if snake['id'] == mySnakeID:
+            mySnake = snake
+            myHe
+            print('My snake is ', mySnake)
         else:
-            enemysnakes.append(snek)
+            otherSnakes.append(snake)
 
-        for coord in snek['body']['data']:
+        snakeCoords = []
+        for coord in snake['body']['data']:
             grid[coord['x']][coord['y']] = SNAKE
-            snekCoords.append([coord['x'],coord['y']])
-        snek['coords'] = snekCoords
-    for food in data['food']['data']:
-        if(snek['health']<40):
-            grid[food['x']][food['y']] = FOOD
+            snakeCoords.append([coord['x'],coord['y']])
+        snek['coords'] = snakeCoords
 
-        else:
-            grid[food['x']-1][food['y']-1] = FOOD +1
-            grid[food['x']-1][food['y']+1] = FOOD+1
-            grid[food['x']+1][food['y']-1] = FOOD+1
-            grid[food['x']+1][food['y']+1] = FOOD+1
+    for food in foods:
+        #if(snek['health'] < 40):
+        grid[food['x']][food['y']] = FOOD
 
-    return mysnake, enemysnakes, grid
+        #else:
+        #    grid[food['x'] - 1][food['y'] - 1] = FOOD + 1
+        #    grid[food['x'] - 1][food['y'] + 1] = FOOD + 1
+        #    grid[food['x'] + 1][food['y'] - 1] = FOOD + 1
+        #    grid[food['x'] + 1][food['y'] + 1] = FOOD + 1
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -94,22 +74,17 @@ def static(path):
 
 @bottle.get('/')
 def index():
-    print ('WORKING ON GET REQUEST')
-    head_url = '%s://%s/static/Traitor.gif' % (
-        bottle.request.urlparts.scheme,
-        bottle.request.urlparts.netloc
-    )
+    print('WORKING ON GET REQUEST')
+    head_url = '%s://%s/static/Traitor.gif' % (bottle.request.urlparts.scheme,
+        bottle.request.urlparts.netloc)
     return {
         'color': '#ff0000',
         'head': head_url
     }
+
 @bottle.post('/start')
 def start():
-    data = bottle.request.json
-
-    # TODO: Do things with data
-    print ('WORKING ON START REQUEST')
-
+    print('WORKING ON START REQUEST')
     return {
         'name': 'RomanInitial',
         'color': '#FF0000',
@@ -121,37 +96,45 @@ def start():
 
 @bottle.post('/move')
 def move():
-    data = bottle.request.json
-    #print (data)
-    print ('WORKING ON MOVE REQUEST')
-    snek, enemysnakes, grid = init(data)
+    global width
+    global height
+    global data
+    global mySnake
+    global mySnakeId
+
+    print('WORKING ON MOVE REQUEST')
+    postData = bottle.request.json
+    snek, otherSnakes, grid = init(postData)
     ID = snek['id']
     ourHealth = snek['health']
-    for enemy in enemysnakes:
+    for enemy in otherSnakes:
         #if (enemy['id'] == ID):
         #    ourHealth = enemy['health']
         #    continue
               
-        #print "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"        
-        #grid[enemy['coords'][0][0]*2-enemy['coords'][1][0]][enemy['coords'][0][1]*2 - enemy['coords'][1][1]] = GOLD
-        #data['gold'].append([enemy['coords'][0][0]*2 - enemy['coords'][1][0],enemy['coords'][0][1]*2 - enemy['coords'][1][1]])
+        #print "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+        #grid[enemy['coords'][0][0]*2-enemy['coords'][1][0]][enemy['coords'][0][1]*2
+        #- enemy['coords'][1][1]] = GOLD
+        #data['gold'].append([enemy['coords'][0][0]*2 -
+        #enemy['coords'][1][0],enemy['coords'][0][1]*2 -
+        #enemy['coords'][1][1]])
 
    
         #if distance(snek['coords'][0], enemy['coords'][0]) > SNEK_BUFFER:
         #    continue
 
        
-        if (enemy['length'] >= snek['length']-1):
+        if (enemy['length'] >= snek['length'] - 1):
             #dodge
             try:
-                if enemy['coords'][0][1] < data['height']-1:
-                    grid[enemy['coords'][0][0]][enemy['coords'][0][1]+1] = SAFTEY             
+                if enemy['coords'][0][1] < data['height'] - 1:
+                    grid[enemy['coords'][0][0]][enemy['coords'][0][1] + 1] = SAFTEY             
                 if enemy['coords'][0][1] > 0:
-                    grid[enemy['coords'][0][0]][enemy['coords'][0][1]-1] = SAFTEY
-                if enemy['coords'][0][0] < data['width']-1:
-                    grid[enemy['coords'][0][0]+1][enemy['coords'][0][1]] = SAFTEY
+                    grid[enemy['coords'][0][0]][enemy['coords'][0][1] - 1] = SAFTEY
+                if enemy['coords'][0][0] < data['width'] - 1:
+                    grid[enemy['coords'][0][0] + 1][enemy['coords'][0][1]] = SAFTEY
                 if enemy['coords'][0][0] > 0:
-                    grid[enemy['coords'][0][0]-1][enemy['coords'][0][1]] = SAFTEY
+                    grid[enemy['coords'][0][0] - 1][enemy['coords'][0][1]] = SAFTEY
             except:
                 pass    
             try:
@@ -159,35 +142,35 @@ def move():
             except:
                 pass
             try:
-                grid[enemy['coords'][0][0]][enemy['coords'][0][1]+1] = SAFTEY
+                grid[enemy['coords'][0][0]][enemy['coords'][0][1] + 1] = SAFTEY
             except:
                 pass
             try:
-                grid[enemy['coords'][0][0]][enemy['coords'][0][1]-1] = SAFTEY
+                grid[enemy['coords'][0][0]][enemy['coords'][0][1] - 1] = SAFTEY
             except:
                 pass
             try:
-                grid[enemy['coords'][0][0]+1][enemy['coords'][0][1]] = SAFTEY
+                grid[enemy['coords'][0][0] + 1][enemy['coords'][0][1]] = SAFTEY
             except:
                 pass
             try:
-                grid[enemy['coords'][0][0]-1][enemy['coords'][0][1]] = SAFTEY
+                grid[enemy['coords'][0][0] - 1][enemy['coords'][0][1]] = SAFTEY
             except:
                 pass
             try:
-                grid[enemy['coords'][0][0]+1][enemy['coords'][0][1]+1] = SAFTEY
+                grid[enemy['coords'][0][0] + 1][enemy['coords'][0][1] + 1] = SAFTEY
             except:
                 pass
             try:
-                grid[enemy['coords'][0][0]-1][enemy['coords'][0][1]-1] = SAFTEY
+                grid[enemy['coords'][0][0] - 1][enemy['coords'][0][1] - 1] = SAFTEY
             except:
                 pass
             try:
-                grid[enemy['coords'][0][0]+1][enemy['coords'][0][1]-1] = SAFTEY
+                grid[enemy['coords'][0][0] + 1][enemy['coords'][0][1] - 1] = SAFTEY
             except:
                 pass
             try:
-                grid[enemy['coords'][0][0]-1][enemy['coords'][0][1]+1] = SAFTEY
+                grid[enemy['coords'][0][0] - 1][enemy['coords'][0][1] + 1] = SAFTEY
             except:
                 pass
             grid[enemy['coords'][1][0]][enemy['coords'][1][1]] = 1
@@ -218,7 +201,8 @@ def move():
     #print('grid is ',grid)
     snek_head = snek['coords'][0]
     #print('snekHead is ', snek['coords'][0])
-    #print('snek head makred as ', grid[snek['coords'][0][0]][snek['coords'][0][1]])
+    #print('snek head makred as ',
+    #grid[snek['coords'][0][0]][snek['coords'][0][1]])
     snek_neck = snek['coords'][1]
     snek_coords = snek['coords']
     print('snake coords are ', snek_coords)
@@ -227,31 +211,31 @@ def move():
     #foods = sorted(data['food'], key = lambda p: distance(p,snek_head ))
     #golds = sorted(data['gold'], key = lambda p: distance(p,snek_head ))
 
-    bestScore=4
+    bestScore = 4
     if (ourHealth > 100):
-        bestScore=10
+        bestScore = 10
         
-    bestGoals=[]
+    bestGoals = []
     print('best goals are',bestGoals)
     #print grid
     for col in xrange(data['height']):
         for row in xrange(data['width']):
-            if grid[row][col]> bestScore:
+            if grid[row][col] > bestScore:
                 bestScore = grid[row][col]
                 
     for col in xrange(data['height']):
         for row in xrange(data['width']):
-            if grid[row][col]== bestScore:
+            if grid[row][col] == bestScore:
                 bestGoals.append([row,col])
                 
     #print "BS",bestScore
-    #print "BG",bestGoals   
+    #print "BG",bestGoals
     
     ##print foods
     #if data['mode'] == 'advanced':
         #foods = data['gold'] + foods #+ heads
         #print("")
-    foods = sorted(bestGoals, key = lambda p: distance(p,snek_head ))
+    foods = sorted(bestGoals, key = lambda p: distance(p,snek_head))
     print('best goals are ', foods)
         
     for food in foods:
@@ -265,7 +249,7 @@ def move():
         print('SNEK HEAD ',snek_head)
         print('SNEK COORDS',snek_coords)
         if not tentative_path:
-            print ('no path to food')
+            print('no path to food')
             continue
 
         print('for food ', food)
@@ -329,7 +313,7 @@ def move():
     if despair:
         for neighbour in neighbours(snek_head,grid,0,snek_coords, [1,2,3]):
             path = a_star(snek_head, neighbour, grid, snek_coords)
-            print ('i\'m scared')
+            print('i\'m scared')
             break
 
     despair = not (path and len(path) > 1)
@@ -338,7 +322,7 @@ def move():
     if despair:
         for neighbour in neighbours(snek_head,grid,0,snek_coords, [1,2]):
             path = a_star(snek_head, neighbour, grid, snek_coords)
-            print ('like so scared')
+            print('like so scared')
             break
 
     print('path before asserts ', path)
@@ -366,12 +350,12 @@ def move():
         except:
             pass
         try:
-            if (grid[snek['coords'][0][0]][snek['coords'][0][1]+1] != 1):
+            if (grid[snek['coords'][0][0]][snek['coords'][0][1] + 1] != 1):
                 moveTo = "down"
         except:
             pass
         try:
-            if (grid[snek['coords'][0][0]][snek['coords'][0][1]-1] != 1):
+            if (grid[snek['coords'][0][0]][snek['coords'][0][1] - 1] != 1):
                 moveTo = "up"
         except:
             pass
@@ -392,6 +376,42 @@ def end():
         'taunt': 'amen!'
     }
 
+def distance(p, q):
+    dx = abs(p[0] - q[0])
+    dy = abs(p[1] - q[1])
+    return dx + dy
+
+def closest(items, start):
+    closest_item = None
+    closest_distance = 10000
+
+    # TODO: use builtin min for speed up
+    for item in items:
+        item_distance = distance(start, item)
+        if item_distance < closest_distance:
+            closest_item = item
+            closest_distance = item_distance
+
+    return closest_item
+
+def direction(from_cell, to_cell):
+    dx = to_cell[0] - from_cell[0]
+    dy = to_cell[1] - from_cell[1]
+    #print ('FROM CELL ',from_cell)
+    #print ('TO CELL ',to_cell)
+    #print ('DX=',dx, ' DY= ',dy)
+    if dx == 1:
+        print('RIGHT')
+        return 'right'
+    elif dx == -1:
+        print('LEFT')
+        return 'left'
+    elif dy == -1:
+        print('UP')
+        return 'up'
+    elif dy == 1:
+        print('DOWN')
+        return 'down'
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
